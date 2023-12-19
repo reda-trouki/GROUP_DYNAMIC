6fbfbc58862a28ba919f2265d674ecb8f250e771
RedaTR <trouki.reda@gmail.com>
2023-12-19 18:27:00 +0100

    creating the content page

--- front-end/src/components/courses/elements/Elements.jsx
+++ front-end/src/components/courses/elements/Elements.jsx
@@ -7,7 +7,7 @@ const Sections = () => {
         <p className="font-semibold text-xl">My Learning : </p>
         <div className="my-2">
           <NavLink to="." end className="sections-btn">
-            In Progress
+            In Progress h
           </NavLink>
           <NavLink to="completed" className="sections-btn">
             Completed
